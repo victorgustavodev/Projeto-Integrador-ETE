0b809740cace9b55cd2cf4600d1607e4ea25cca7
Williane Nascimento <101845432+willianenf@users.noreply.github.com>
2023-11-25 18:27:37 -0300

    att cadastro

--- php/cadastro.php
+++ php/cadastro.php
@@ -3,18 +3,17 @@ session_start();
 include_once("conexao.php");
 
 $nome = filter_input(INPUT_POST, 'nome');
-$telefone = filter_input(INPUT_POST, 'telefone');
 $email = filter_input(INPUT_POST, 'email');
 $senha_usuario = password_hash(filter_input(INPUT_POST, 'senha_usuario'), PASSWORD_DEFAULT);
 
-$query_usuario = "INSERT INTO usuarios (nome, telefone, email, senha_usuario, created) VALUES ('$nome', '$telefone', '$email', '$senha_usuario', NOW())";
+$query_usuario = "INSERT INTO usuarios (nome, email, senha_usuario, created) VALUES ('$nome', '$email', '$senha_usuario', NOW())";
 $resultado_usuario  = $conn->prepare($query_usuario);
 $resultado_usuario-> execute();
 
 if(($resultado_usuario) AND ($resultado_usuario->rowCount() != 0)){
-    header("Location: index.html");
-    $_SESSION['msg'] = "<p style='font-size: 40px; color: green;'>Cadastro realizado! Favor logar para ter acesso total ao nosso site!</p>";
+    header("Location: ../login.php");
+    $_SESSION['msg'] = "<p style='font-size: 15px; color: green;'>Cadastro realizado! Favor logar para ter acesso total ao nosso site!</p>";
 } else{
-    header("Location: registro.php");
-    $_SESSION['msg'] = "<p style='font-size: 20px; color: #ff0000;'>Cadastro não foi realizado, favor verificar se dados foram preenchidos corretamente!</p>";
+    header("Location: register.php");
+    $_SESSION['msg'] = "<p style='font-size: 15px; color: #ff0000;'>Cadastro não foi realizado, favor verificar se dados foram preenchidos corretamente!</p>";
 }
