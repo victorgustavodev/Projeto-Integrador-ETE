01fb83ea495c4e9618755158795de0d03f490a4a
italo <italosport110@hotmail.com>
2023-11-26 16:45:33 -0300

    feat: add effect for image

--- js/carrossel.js
+++ js/carrossel.js
@@ -2,42 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   let currentSlide = 1;
   showSlide(currentSlide);
 
-  setInterval(function () {
-    currentSlide++;
-    if (currentSlide > 3) {
-      currentSlide = 1;
-    }
-    showSlide(currentSlide);
-  }, 3000);
-
-  function showSlide(slideIndex) {
-    const slides = document.getElementsByClassName("carrossel-slide");
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-
-    // Adiciona efeito de sombra ao passar o mouse
-    const images = slides[slideIndex - 1].getElementsByTagName("img");
-    for (let i = 0; i < images.length; i++) {
-      images[i].addEventListener("mouseenter", addShadow);
-      images[i].addEventListener("mouseleave", removeShadow);
-    }
-  }
-
-  function addShadow() {
-    this.classList.add("image-hovered");
-  }
-
-  function removeShadow() {
-    this.classList.remove("image-hovered");
-  }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  let currentSlide = 1;
-  showSlide(currentSlide);
-
   // Add event listeners for the buttons
   const prevButton = document.getElementById("prevButton");
   const nextButton = document.getElementById("nextButton");
@@ -47,12 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setInterval(function () {
     changeSlide(1);
-  }, 3000);
+  }, 5000);
 
   function showSlide(slideIndex) {
     const slides = document.getElementsByClassName("carrossel-slide");
     for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
+      // slides[i].style.opacity = "1";
     }
     slides[slideIndex - 1].style.display = "block";
   }
